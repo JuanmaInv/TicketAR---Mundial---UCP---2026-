00f3f40fb084d8d31a94686611b0ce3aeb52afcd
ErwinPz <pzocikerwin@gmail.com>
2026-04-13 15:19:33 -0300

    refactor: remove default nestjs boilerplate from app.module (refactorizacion de codigo)

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [],
+  imports: [TicketsModule],
   controllers: [AppController],
   providers: [AppService],
 })
