343ccf499cf2615b7d8673ac55d6ce6c8678a69b
ErwinPz <pzocikerwin@gmail.com>
2026-04-19 23:50:54 -0300

    fix(matches): register providers

--- backend-nest/src/matches/matches.module.ts
+++ backend-nest/src/matches/matches.module.ts
@@ -1,9 +1,4 @@
 import { Module } from '@nestjs/common';
-import { MatchesController } from './matches.controller';
-import { MatchesService } from './matches.service';
 
-@Module({
-  controllers: [MatchesController],
-  providers: [MatchesService],
-})
+@Module({})
 export class MatchesModule {}
