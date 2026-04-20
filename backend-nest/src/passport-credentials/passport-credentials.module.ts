4cd08faa993479e1d2f9202898ed46c5c55b5db1
ErwinPz <pzocikerwin@gmail.com>
2026-04-19 23:50:56 -0300

    fix(passport): register providers

--- backend-nest/src/passport-credentials/passport-credentials.module.ts
+++ backend-nest/src/passport-credentials/passport-credentials.module.ts
@@ -1,9 +1,4 @@
 import { Module } from '@nestjs/common';
-import { PassportCredentialsController } from './passport-credentials.controller';
-import { PassportCredentialsService } from './passport-credentials.service';
 
-@Module({
-  controllers: [PassportCredentialsController],
-  providers: [PassportCredentialsService],
-})
+@Module({})
 export class PassportCredentialsModule {}
