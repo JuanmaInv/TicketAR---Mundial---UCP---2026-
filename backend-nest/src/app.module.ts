e7f47436253c1a1ae102f867723454d35615f271
ErwinPz <pzocikerwin@gmail.com>
2026-04-19 23:58:01 -0300

    fix: clean up app.module

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { TicketsModule } from './tickets/tickets.module';
 import { PaymentsModule } from './payments/payments.module';
 import { PassportCredentialsModule } from './passport-credentials/passport-credentials.module';
-import { ReservationsModule } from './reservations/reservations.module';
 import { StadiumSectorsModule } from './stadium-sectors/stadium-sectors.module';
 import { MatchesModule } from './matches/matches.module';
 
 
 //MODULO RAIZ QUE CONECTA A TODOS LOS MODULOS ANTERIORES
 @Module({
-  imports: [TicketsModule, PaymentsModule, PassportCredentialsModule, ReservationsModule, StadiumSectorsModule, MatchesModule],
+  imports: [
+    TicketsModule,
+    PaymentsModule,
+    PassportCredentialsModule,
+    StadiumSectorsModule,
+    MatchesModule
+  ],
   controllers: [],
   providers: [],
 })
